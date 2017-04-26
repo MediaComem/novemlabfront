45c7b3f61dbe240dd9fdc76abcba25e096593a4a
Xavier <xavier.aeby@outlook.com>
2017-04-26 11:18:59 +0200

    novem message update

---
+++ public/javascripts/scripts/novemMessage.js
@@ -0,0 +1,20 @@
+function novemCall (message, nodeDom) {
+        nodeDome.empty();		
+        indChar = 0;
+        function changeMessage(){
+            nodeDom.append(message.substr(indChar, 1));
+            indChar++;
+            if (indChar < message.length) setTimeout(changeMessage, 70);
+        }
+        setTimeout(function(){
+            setTimeout(changeMessage, 70);
+        },800);
+	}
+
+$(function(){ 
+    var message = $("#novemText").html();
+    $("#novemText").empty();	
+    novemCall(message, $('#novemText'));
+
+})
+
