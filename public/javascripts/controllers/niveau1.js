2bf4f067f071b8ea80731030d7f0b55ec775d1f4
randsom <sandoz.romain@gmail.com>
2017-04-27 14:34:16 +0200

    niveau1ctrl

--- public/javascripts/controllers/niveau1.js
+++ public/javascripts/controllers/niveau1.js
@@ -1,42 +1,50 @@
 
 angular.module('novemlab').controller('N1Controller', function(apiUrl, $scope, $state, $http) {
     var n1Ctrl = this;
-    console.log(n1Ctrl.test);
-    
-$(document).ready(function(){
-$('button.versNiveau').hide();
-  var limit = 3;
-  
-	$('.tools .col-xs-4').click(function(e){
-    	e.preventDefault();
-    
-    if($(".tools .col-xs-4.active").length >= limit) {
-      if($(this).hasClass("active"))
-      {
-      $(this).toggleClass("active");
-    		$('button.versNiveau').hide();
-      }
-      
-    }else{
-     	$(this).toggleClass("active");
-   
-    	var occ = $('.active').length; 
-    	if (occ == 3){
-    		$('button.versNiveau').show();
-    		
-    		// Lors de l'envoi récupère les trois outils choisis
-    		$('button.versNiveau').click(function(){
-    			$( ".col-xs-4.active" ).each(function( index ) {
-  console.log( index + ": " + $( this ).text() );
-});
-			});
-    		
-    	}
-    	
-    }
-    
-  });
-  
-});
+    niveau = 1;
 
+   /* n1Ctrl.etape = {} ;*/
+
+    init = function(){
+
+        /**
+         * GET DATA
+         */
+        /*TODO*/
+
+
+        /**
+         * GENERATE FRONT
+         */
+        $('button.versNiveau').hide();
+        var limit = 3;
+
+        $('.tools .col-xs-4').click(function(e){
+            e.preventDefault();
+
+            if($(".tools .col-xs-4.active").length >= limit) {
+                if($(this).hasClass("active"))
+                {
+                    $(this).toggleClass("active");
+                    $('button.versNiveau').hide();
+                }
+
+            }else{
+                $(this).toggleClass("active");
+
+                var occ = $('.active').length;
+                if (occ == 3){
+                    $('button.versNiveau').show();
+
+                    // Lors de l'envoi récupère les trois outils choisis
+                    $('button.versNiveau').click(function(){
+                        $( ".col-xs-4.active" ).each(function( index ) {
+                            console.log( index + ": " + $( this ).text() );
+                        });
+                    });
+                }
+            }})
+    };
+
+    init();
 });
