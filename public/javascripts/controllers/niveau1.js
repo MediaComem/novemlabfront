488c9282b2fbacde3d4cfd26bfa5b80466a8cb5d
randsom <sandoz.romain@gmail.com>
2017-05-01 17:33:11 +0200

    Fixed n2 for tools interaction

--- public/javascripts/controllers/niveau1.js
+++ public/javascripts/controllers/niveau1.js
@@ -3,57 +3,61 @@ angular.module('novemlab').controller('N1Controller', function(EtapeService, Jou
     var n1Ctrl = this;
     niveau = 1;
 
-   n1Ctrl.etape = {};
-   n1Ctrl.niveau = "1";
+    n1Ctrl.etape = {};
+    n1Ctrl.niveau = "1";
 
-    init = function(){
+    /**
+     * GET DATA
+     */
+    EtapeService.show(n1Ctrl.niveau).then(function(){
+        n1Ctrl.etape = EtapeService.getEtape();
+    })
+
+    /**
+     * GENERATE FRONT
+     */
+    $('button.versNiveau').hide();
+    var limit = 3;
+
+    $(document).on("click", '.tools .col-xs-6.col-md-4', function(e){
+        e.preventDefault();
+
+        if($(".tools .col-xs-6.col-md-4.active").length >= limit) {
+            if($(this).hasClass("active"))
+            {
+                $(this).toggleClass("active");
+                $('button.versNiveau').hide();
+            }
+
+        }else{
+            $(this).toggleClass("active");
+
+            var occ = $('.active').length;
+            if (occ == 3){
+                $('button.versNiveau').show();
+
+                // Lors de l'envoi récupère les trois outils choisis
+                $('button.versNiveau').click(function(){
+                    $( ".col-xs-6.col-md-4.active" ).each(function( index ) {
+                        console.log( index + ": " + $( this ).text() );
+                    });
+                });
+            }
+        }})
+
+
+    var save =function(){
 
         /**
-         * GET DATA
+         * TODO - RéCUPéRER LE SCORE
          */
-        EtapeService.show(n1Ctrl.niveau).then(function(){
-            n1Ctrl.etape = EtapeService.getEtape();
+        JoueurService.updateScorePhase1().then(function(){
+
+            $window.location.href = "/n2";
         })
 
-        /**
-         * GENERATE FRONT
-         */
-        $('button.versNiveau').hide();
-        var limit = 3;
-
-        $('.tools .col-xs-6.col-md-4').click(function(e){
-            e.preventDefault();
-
-            if($(".tools .col-xs-6.col-md-4.active").length >= limit) {
-
-                if($(this).hasClass("active"))
-                {
-                    $(this).toggleClass("active");
-                    $('button.versNiveau').hide();
-                }
-
-            }else{
-                $(this).toggleClass("active");
-
-                var occ = $('.active').length;
-                if (occ == 3){
-                    $('button.versNiveau').show();
-
-                    // Lors de l'envoi récupère les trois outils choisis
-                    $('button.versNiveau').click(function(){
-                        $( ".col-xs-6.col-md-4.active" ).each(function( index ) {
-                            console.log( index + ": " + $( this ).text() );
-                        });
-                    });
-                }
-            }})
-    };
-
-    function save(){
-        JoueurService.updateScorePhase1
     }
 
-    init();
 
 }).filter('removeAccent', function(){
         return function (source) {
