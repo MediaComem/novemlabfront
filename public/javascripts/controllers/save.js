e646b63982361ead5de298214b976a2f8263501e
randsom <sandoz.romain@gmail.com>
2017-05-02 15:12:00 +0200

    Score creation fixed

--- public/javascripts/controllers/save.js
+++ public/javascripts/controllers/save.js
@@ -1,8 +1,11 @@
 
-angular.module('novemlab').controller('SaveController', function(JoueurService, $window, apiUrl, $scope, $state, $http) {
+angular.module('novemlab').controller('SaveController', function(JoueurService, apiUrl, $scope, $state, $http, $window) {
     var sCtrl = this;
 
     sCtrl.test = "coucou";
+    sCtrl.joueur = JSON.parse($window.sessionStorage.getItem("joueur"));
+    sCtrl.score = JSON.parse($window.sessionStorage.getItem("score"));
+    console.log(sCtrl.score);
 
     //appel au service//
     JoueurService.showAll().then(function(){    console.log(JoueurService.getJoueurs());
