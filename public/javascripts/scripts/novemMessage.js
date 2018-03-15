d1d0a20435078e807b76c37c1cbf9476d94db93c
DESKTOP-1L2CP4G\Romain S <romain.sandoz@heig-vd.ch>
2018-03-15 17:33:44 +0100

    Fix novemVoice

--- public/javascripts/scripts/novemMessage.js
+++ public/javascripts/scripts/novemMessage.js
@@ -1,22 +1,24 @@
-function novemCall (message, nodeDom) {
+$("button.closeNovemVoice").css("opacity",0);
+
+function novemCall(message, nodeDom) {
         nodeDom.empty();
         indChar = 0;
         function changeMessage(){
             nodeDom.append(message.substr(indChar, 1));
             indChar++;
-            if (indChar < message.length) setTimeout(changeMessage, 20);
+            if (indChar < message.length){setTimeout(changeMessage, 20)}else{$("button.closeNovemVoice").fadeTo("slow",1);
+            } ;
         }
         setTimeout(function(){
             setTimeout(changeMessage, 10);
         },400);
-	}
+}
 
 function showMessage(){
     var message = $("#novemText").html();
     /*$("#novemText").empty();*/
     novemCall(message, $('#novemText'));
 }
-
 
 $('figure.ball').click(function(){
  	$('.novemVoice').fadeToggle( "fast", "linear" );;
