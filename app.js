1154e5d5dcbcebd0b71faef849766f3d731d2aa9
Imagoiq <git.noreply@posteo.no>
2020-04-01 12:24:12 +0200

    Upgrade packages

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var proxy = require('express-http-proxy');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //controllers.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
