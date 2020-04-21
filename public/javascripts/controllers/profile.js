85a4deca53461820980a7a3aa96600973b1b1b20
Imagoiq <git.noreply@posteo.no>
2020-04-21 12:12:05 +0200

    Fix modal role button and margin

--- public/javascripts/controllers/profile.js
+++ public/javascripts/controllers/profile.js
@@ -10,71 +10,74 @@ angular.module('novemlab').controller('NFController', function(apiUrl, $scope, $
     const scaleScoreMin = 0;
     const scaleScoreMax = 10;
 
-    /* Calculate Polygon Points */
 
-    var poly = [
-        scoreTransform(nfCtrl.score.communication, '.scale__diagonalpos', 'afterMiddle')
-            .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
-        scoreTransform(nfCtrl.score.management, '.scale__horizontal', 'afterMiddle')
-            .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
-        scoreTransform(nfCtrl.score.business, '.scale__diagonalneg', 'afterMiddle')
-            .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
-        scoreTransform(nfCtrl.score.gestion, '.scale__diagonalpos', 'beforeMiddle')
-            .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
-        scoreTransform(nfCtrl.score.conception, '.scale__horizontal', 'beforeMiddle')
-            .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
-        scoreTransform(nfCtrl.score.technique, '.scale__diagonalneg', 'beforeMiddle')
-            .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates()
-    ].map(function (d) {
-        return [d.x, d.y].join(",");
-    }).join(" ");
+    if (nfCtrl.score !== null) {
 
-    function scoreTransform(score, axeClass, axePosition){
-        var self = this;
-        self._score = score;
-        self._axeNode = document.querySelector(axeClass);
-        self._axePosition = axePosition;
+        /* Calculate Polygon Points */
 
-        return {
-            toPoint: function (scoreMin, scoreMax, check){
-                var s_length = self._axeNode.getTotalLength();
+        var poly = [
+            scoreTransform(nfCtrl.score.communication, '.scale__diagonalpos', 'afterMiddle')
+                .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
+            scoreTransform(nfCtrl.score.management, '.scale__horizontal', 'afterMiddle')
+                .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
+            scoreTransform(nfCtrl.score.business, '.scale__diagonalneg', 'afterMiddle')
+                .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
+            scoreTransform(nfCtrl.score.gestion, '.scale__diagonalpos', 'beforeMiddle')
+                .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
+            scoreTransform(nfCtrl.score.conception, '.scale__horizontal', 'beforeMiddle')
+                .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates(),
+            scoreTransform(nfCtrl.score.technique, '.scale__diagonalneg', 'beforeMiddle')
+                .toPoint(scaleScoreMin, scaleScoreMax).toPathCoordinates()
+        ].map(function (d) {
+            return [d.x, d.y].join(",");
+        }).join(" ");
 
-                var rangeAxeMin = axePosition === "afterMiddle" ? s_length / 2 : s_length / 2;
-                var rangeAxeMax = axePosition === "afterMiddle" ? s_length : 0;
+        function scoreTransform(score, axeClass, axePosition){
+            var self = this;
+            self._score = score;
+            self._axeNode = document.querySelector(axeClass);
+            self._axePosition = axePosition;
 
-                var scale = new Scale().domain(scoreMin, scoreMax).range(rangeAxeMin, rangeAxeMax);
-                self._scorePoint = scale(self._score);
+            return {
+                toPoint: function (scoreMin, scoreMax, check){
+                    var s_length = self._axeNode.getTotalLength();
 
-                return this
-            },
-            toPathCoordinates: function (){
-                return self._axeNode.getPointAtLength(self._scorePoint);
+                    var rangeAxeMin = axePosition === "afterMiddle" ? s_length / 2 : s_length / 2;
+                    var rangeAxeMax = axePosition === "afterMiddle" ? s_length : 0;
+
+                    var scale = new Scale().domain(scoreMin, scoreMax).range(rangeAxeMin, rangeAxeMax);
+                    self._scorePoint = scale(self._score);
+
+                    return this
+                },
+                toPathCoordinates: function (){
+                    return self._axeNode.getPointAtLength(self._scorePoint);
+                }
             }
         }
-    }
 
-    /* Draw Polygon Points */
+        /* Draw Polygon Points */
 
-    var polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    polygon.setAttribute("points", poly)
-    polygon.setAttribute("fill", "rgba(var(--heig-red-rgb), .8)");
+        var polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        polygon.setAttribute("points", poly)
+        polygon.setAttribute("fill", "rgba(var(--heig-red-rgb), .8)");
 
-    document.querySelector(".schemaProfile__spider_background").appendChild(polygon);
+        document.querySelector(".schemaProfile__spider_background").appendChild(polygon);
 
-    /* Unwrap text label on desktop */
-    var schemaProfile = document.querySelector(".schemaProfile svg");
-    window.addEventListener('resize', setViewbox);
-    setViewbox();
+        /* Unwrap text label on desktop */
+        var schemaProfile = document.querySelector(".schemaProfile svg");
+        window.addEventListener('resize', setViewbox);
+        setViewbox();
 
-    function setViewbox() {
-        if (window.innerWidth > 768){
-            schemaProfile.setAttribute("viewBox", "-130 0 600 298");
-        } else{
-            schemaProfile.setAttribute("viewBox", "0 0 350 298");
+        function setViewbox() {
+            if (window.innerWidth > 768){
+                schemaProfile.setAttribute("viewBox", "-130 0 600 298");
+            } else{
+                schemaProfile.setAttribute("viewBox", "0 0 350 298");
+            }
         }
+
     }
-
-
 
 
     /*  Scale function
